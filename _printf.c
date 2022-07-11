83ed9029e01d3165f1001b58fcd8edbd36bdb729
OttoNyongole <nyongole.otto@gmail.com>
2022-07-11 16:31:13 +0300

    simple modifications

--- _printf.c
+++ _printf.c
@@ -1,47 +1,31 @@
-#include <stdarg.h>
-#include <stdlib.h>
 #include "main.h"
+
 /**
- * _printf - prints according to format
- * @format: The given format
- *
- * Return: On success 1.
- * On error, -1 is returned.
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formated string
+ * @format: A string containing all the desired characters
+ * Return: A total count of the characters printed
  */
 int _printf(const char *format, ...)
 {
-	va_list params;
-	printer_t current_printer;
-	int i = 0, putchar_flag = 1, length = 0;
+	int printed_chars;
+	conver_t f_list[] = {
 
-	if ((format == NULL) || (format[0] == '%' && !format[1]))
+		{"c", print_char},
+
+		{"s", print_string},
+
+		{"%", print_percent},
+		{NULL, NULL}
+	};
+	va_list arg_list;
+
+	if (format == NULL)
 		return (-1);
-	va_start(params, format);
-	while (format[i] != '\0')
-	{
-		if (format[i] != '%')
-			if (putchar_flag)
-				length += _putchar(format[i]);
-			else
-			{
-				current_printer = select_printer(format[i]);
-				if (current_printer.format != '*')
-					length += current_printer.func(&params);
-				else
-					length += _putchar('%') + _putchar(format[i]);
-				putchar_flag = 1;
-			}
-		else
-			if (putchar_flag)
-				putchar_flag = 0;
-			else
-			{
-				/* ToDO: Add condition for blanks */
-				length += _putchar(format[i]);
-				putchar_flag = 1;
-			}
-		i++;
-	}
-	va_end(params);
-	return (length);
+
+	va_start(arg_list, format);
+	/*Calling parser function*/
+	printed_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
 }
